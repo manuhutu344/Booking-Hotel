e8c3044ec338a9a9154d5f6e127971a81c7c338e
manuhutu344 <nandoperdos2@gmail.com>
2023-03-14 08:46:39 +0900

    Membuat Register

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Route, Routes} from 'react-router-dom'
 import Login from './Pages/Login'
 import Home from './Pages/Home'
 import Layout from './Components/Layout'
+import Register from './Pages/Register'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <Route path='/' element={<Layout />}>
     <Route index element={<Home />} />
     <Route path='/login' element={<Login />} />
+    <Route path='/buat' element={<Register />} />
     </Route>
     </Routes>
   )
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function Login() {
+function Register() {
   return (
     <div className='mt-4 grow flex items-center justify-around'>
     <div className='mb-64'>
-    <h1 className='text-4xl text-center mb-4'>Login Ke Akun Anda</h1>
+    <h1 className='text-4xl text-center mb-4'>Buat Akun Anda</h1>
     <form className='max-w-md mx-auto'>
+    <input type='text' placeholder='Masukan Nama Anda' />
     <input type='email' placeholder='Masukan Email Anda'  />
     <input type='password' placeholder='Masukan Password Anda' />
-    <button className='primary'>Login</button>
+    <button className='primary'>Buat</button>
     <div className='text-center py-2 text-gray-500'>
-    Jika Tidak Memiliki Akun.  <Link className='underline text-black' to={'/buat'}>
-    Buat Akun Sekarang
+    Sudah Memiliki Akun?  <Link className='underline text-black' to={'/login'}>
+     Login Sekarang
     </Link> 
     </div>
     </form>
@@ -21,4 +22,4 @@ function Login() {
   )
 }
 
-export default Login
+export default Register
