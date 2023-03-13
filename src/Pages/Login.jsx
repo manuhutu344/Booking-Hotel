52e106222f998a2566772da4f9e98824753a3f77
manuhutu344 <nandoperdos2@gmail.com>
2023-03-13 20:22:53 +0900

    Mengatur jarak antar form

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Ini Loginnya</div>
+    <div className='mt-4'>
+    <h1 className='text-4xl text-center mb-4'>Login Ke Akun Anda</h1>
+    <form className='max-w-md mx-auto'>
+    <input type='email' placeholder='Masukan Email Anda'  />
+    <input type='password' placeholder='Masukan Password Anda' />
+    <button className='primary'>Login</button>
+    </form>
+    </div>
   )
 }
 
