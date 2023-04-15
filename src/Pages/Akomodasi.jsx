767bf69614d25bc26a716ccfcd4141686807e085
manuhutu344 <nandoperdos2@gmail.com>
2023-04-15 11:53:47 +0900

    Membuat agar bisa upload foto menggunakan url

--- src/Pages/Akomodasi.jsx
+++ src/Pages/Akomodasi.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {Link, useParams} from 'react-router-dom'
 import LabelKeuntungan from '../Components/LabelKeuntungan'
+import axios from 'axios'
 
 function Akomodasi() {
   const {action} = useParams()
@@ -32,6 +33,14 @@ function Akomodasi() {
       </>
     )
   }
+  async function addPhotoByLink(e){
+    e.preventDefault()
+    const {data:filename} = await axios.post('/post/upload-by-link', {Link:photoLink})
+    setAddedPhotos(prev =>{
+      return[...prev, filename]
+    })
+    setPhotoLink('')
+  }
   return (
     <div>
     {action !== 'new' && (
@@ -54,9 +63,14 @@ function Akomodasi() {
       {preInput('Foto','Masukan foto dengan benar')}
       <div className='flex gap-2'>
       <input type='text' value={photoLink} onChange={e => setPhotoLink(e.target.value)} placeholder={'Tambahkan menggunakan link ....jpg'} />
-      <button className='bg-gray-200 px-4 rounded-2xl'>Masukan&nbsp;Foto</button>
+      <button onClick={addPhotoByLink} className='bg-gray-200 px-4 rounded-2xl'>Masukan&nbsp;Foto</button>
       </div>
       <div className='mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
+      {addedPhotos.length > 0 && addedPhotos.map(Link =>(
+        <div>
+        {Link}
+        </div>
+      ))}
       <button className='flex gap-1 justify-center border bg-transparent rounded-2xl p-8 text-2xl text-gray-600'>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
