79bd8eb7b71a6d16407a4e6fa9a915daf4f71869
manuhutu344 <nandoperdos2@gmail.com>
2023-04-18 21:28:25 +0900

    Membuat tampilan foto di frontend

--- src/Pages/Akomodasi.jsx
+++ src/Pages/Akomodasi.jsx
@@ -41,6 +41,21 @@ function Akomodasi() {
     })
     setPhotoLink('')
   }
+  function uploadPhoto(e){
+    const files =  e.target.files
+    const data = new FormData()
+    for(let i = 0; i < files.length; i++){
+      data.append('photos', files[i])
+    }
+    axios.post('/post/upload', data, {
+      headers: {'Content-type' : 'multipart/form-data'}
+    }).then(response =>{
+      const {data:filename} = response
+      setAddedPhotos(prev =>{
+        return[...prev, filename]
+      })
+    })
+  }
   return (
     <div>
     {action !== 'new' && (
@@ -68,15 +83,16 @@ function Akomodasi() {
       <div className='mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
       {addedPhotos.length > 0 && addedPhotos.map(Link =>(
         <div>
-        <img className='rounded-2xl' src={'http://localhost:4000/upload'+Link} alt='' />
+        <img className='rounded-2xl' src={'http://localhost:4000/upload/'+Link} alt='' />
         </div>
       ))}
-      <button className='flex gap-1 justify-center border bg-transparent rounded-2xl p-8 text-2xl text-gray-600'>
+      <label className='cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600'>
+      <input type="file" multiple className='hidden' onChange={uploadPhoto} />
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
 </svg>
       Upload
-      </button>
+      </label>
       </div>
       {preInput('Deskripsi','Deskripsikan tempat Anda.')}
       <textarea value={description} onChange={e => setDescription(e.target.value)} />
