ee5ad5a6286823c586683ed2550c21f6775b4d05
manuhutu344 <nandoperdos2@gmail.com>
2023-04-27 21:40:20 +0900

    Membuat Pages baru dan componnet untuk akun

--- src/Pages/Akomodasi.jsx
+++ src/Pages/Akomodasi.jsx
@@ -1,50 +1,8 @@
-import React, { useState } from 'react'
-import {Link, Navigate, useParams} from 'react-router-dom'
-import LabelKeuntungan from '../Components/LabelKeuntungan'
-import axios from 'axios'
-import PhotosUploader from '../Components/PhotosUploader'
+import React  from 'react'
+import {Link , useParams} from 'react-router-dom'
 
 function Akomodasi() {
   const {action} = useParams()
-  const [title, setTitle] = useState('')
-  const [address, setAddress] = useState('')
-  const [addedPhotos, setAddedPhotos] = useState([])
-  const [description, setDescription] = useState('')
-  const [perks, setPerks] = useState([])
-  const [extraInfo, setExtraInfo] = useState('')
-  const [checkIn, setCheckIn] = useState('')
-  const [checkOut, setCheckOut] = useState('')
-  const [maxGuests, setMaxGuests] = useState(1)
-  const [redirect, setRedirect] = useState('')
-  function inputHeader(text){
-    return(
-      <h2 className='text-2xl mt-4'>{text}</h2>
-    )
-  }
-  function inputDescription(text){
-    return(
-      <p className='text-gray-500 text-sm'>{text}</p>
-    )
-  }
-  function preInput(header, description){
-    return(
-      <>
-      {inputHeader(header)}
-      {inputDescription(description)}
-      </>
-    )
-  }
-
-  async function addNewPlace(e){
-    e.preventDefault()
-    const placeData = {title, address, addedPhotos, description, perks, extraInfo, checkIn, checkOut, maxGuests}
-    await axios.post('/post/place', placeData)
-    setRedirect('/akun/akomodasi')
-  }
-
-  if(redirect){
-    return <Navigate to ={redirect} />
-  }
   
   return (
     <div>
@@ -57,48 +15,6 @@ function Akomodasi() {
     Tambahkan Akomodasi Baru Anda
     </Link>
     </div>
-    )}
-    {action === 'new' && (
-      <div>
-      <form onSubmit={addNewPlace}>
-      {preInput('Judul', 'judul untuk tempat Anda. Harus pendek dan menarik seperti dalam iklan')}
-      <input type='text' value={title} onChange={e => setTitle(e.target.value)} placeholder='Masukan Judul' />
-      {preInput('Alamat', 'Masukan Alamat untuk tempat Anda.')}
-      <input type='text' value={address} onChange={e => setAddress(e.target.value)} placeholder='Masukan Alamat Anda' />
-      {preInput('Foto','Masukan foto dengan benar')}
-      <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos} />
-      {preInput('Deskripsi','Deskripsikan tempat Anda.')}
-      <textarea value={description} onChange={e => setDescription(e.target.value)} />
-      {preInput('Keuntungan','Pililah semua keuntungan pada tempat anda')}
-      <div className='grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6'>
-      <LabelKeuntungan selected={perks} onChange={setPerks} />
-      </div>
-      {preInput('Info Tambahan','Aturan dan lain sebagainya')}
-      <textarea value={extraInfo} onChange={e => setExtraInfo(e.target.value)} />
-      {preInput('Check in','Tambahkan Check in')}
-      <div className='grid gap-2 sm:grid-cols-3'>
-      <div>
-      <h3 className='mt-2 -mb-1'>
-      Waktu Check in
-      </h3>
-      <input type='text' value={checkIn} onChange={e => setCheckIn(e.target.value)} placeholder='14:00'/>
-      </div>
-      <div>
-      <h3 className='mt-2 -mb-1'>
-      Waktu Check Out
-      </h3>
-      <input type='text' value={checkOut} onChange={e => setCheckOut(e.target.value)} placeholder='11.00'/>
-      </div>
-      <div>
-      <h3 className='mt-2 -mb-1'>
-      Batas pengunjung
-      </h3>
-      <input type='number' value={maxGuests} onChange={e => setMaxGuests(e.target.value)} />
-      </div>
-      </div>
-      <button className='primary my-4'>Simpan</button>
-      </form>
-      </div>
     )}
     </div>
   )
