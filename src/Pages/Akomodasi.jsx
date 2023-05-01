4ce0833062fcd23eba06d8e423009d582ff36a84
manuhutu344 <nandoperdos2@gmail.com>
2023-05-01 19:48:21 +0900

    Membuat tampilan data dari databases place ke FE

--- src/Pages/Akomodasi.jsx
+++ src/Pages/Akomodasi.jsx
@@ -1,20 +1,44 @@
-import React  from 'react'
+import React, { useEffect, useState }  from 'react'
 import {Link} from 'react-router-dom'
 import AccountNav from '../Components/AccountNav'
+import axios from 'axios'
 
 function Akomodasi() {
+  const [places, setPlaces] = useState([])
+  useEffect(()=>{
+    axios.get('/post/akomodasi').then(({data}) =>{
+      setPlaces(data)
+    })
+  }, [])
   return (
     <div>
     <AccountNav />
       <div className= 'text-center'>
-      Daftar Semua Tempat Yang Ditambahkan
-      <br />
     <Link className='inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full' to={'/akun/akomodasi/new'}>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 </svg>
     Tambahkan Akomodasi Baru Anda
     </Link>
+    </div>
+    <div className='mt-4'>
+    {places.length > 0 && places.map(place => (
+      <Link to={'/akun/akomodasi/' +place._id} className='flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl'>
+      <div className='w-32 h-32 bg-gray-300'>
+      {place.photos.length > 0 && (
+        <img src={place.photos[0]} alt='' />
+      )}
+      </div>
+      <div className='grow-0 shrink'>
+      <h2 className='text-xl'>
+      {place.title}
+      </h2>
+      <p className='text-sm mt-2'>
+      {place.description}
+      </p>
+      </div>
+      </Link>
+    ))}
     </div>
     </div>
   )
