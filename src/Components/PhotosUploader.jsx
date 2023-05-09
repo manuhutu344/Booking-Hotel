48bc09e6b181427d70e8c0fab04c7f664e934a21
manuhutu344 <nandoperdos2@gmail.com>
2023-05-09 20:19:04 +0900

    Menampilkan data ke home

--- src/Components/PhotosUploader.jsx
+++ src/Components/PhotosUploader.jsx
@@ -28,12 +28,12 @@ function PhotosUploader({addedPhotos, onChange}) {
       }
 
       function removePhoto(e, filename){
-        e.preventDefault
+        e.preventDefault()
         onChange([...addedPhotos.filter(photo => photo !== filename)])
       }
 
       function selectASMainPhoto(e, filename){
-        e.preventDefault
+        e.preventDefault()
         const addedPhotosWithoutSelected = addedPhotos.filter(photo => photo !== filename)
         const newAddedPhotos = [filename, ...addedPhotosWithoutSelected]
         onChange(newAddedPhotos)
@@ -49,12 +49,12 @@ function PhotosUploader({addedPhotos, onChange}) {
       {addedPhotos.length > 0 && addedPhotos.map(Link =>(
         <div className='h-32 flex relative' key={Link}>
         <img className='rounded-2xl w-full object-cover' src={'http://localhost:4000/upload/'+Link} alt='' />
-        <button onClick={(e) => removePhoto(e,Link)} className='cursor-pointer absolute bottom-1 right-1 text-white bg-black bg-opacity-50 rounded-2xl py-2 px-3'>
+        <button onClick={e => removePhoto(e,Link)} className='cursor-pointer absolute bottom-1 right-1 text-white bg-black bg-opacity-50 rounded-2xl py-2 px-3'>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
 </svg>
         </button>
-        <button onClick={(e) => selectASMainPhoto(e,Link)} className='cursor-pointer absolute bottom-1 left-1 text-white bg-black bg-opacity-50 rounded-2xl py-2 px-3'>
+        <button onClick={e => selectASMainPhoto(e,Link)} className='cursor-pointer absolute bottom-1 left-1 text-white bg-black bg-opacity-50 rounded-2xl py-2 px-3'>
         {Link === addedPhotos[0] && (
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
   <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [places, setPlaces] = useState([])
+  useEffect(()=>{
+    axios.get('/post/data').then(response => {
+      setPlaces(response.data)
+    })
+  }, [])
   return (
-    <div>
-    Ini Home
+    <div className='mt-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+    {places.length > 0 && places.map(place => (
+      <div>
+      <div className='bg-gray-500 rounded-2xl flex'>
+      {place.photos?.[0] && (
+        <img className='rounded-2xl' src={'http://localhost:4000/upload/'+place.photos?.[0]} alt='' />
+      )}
+      </div>
+      {place.title}
+      </div>
+    ))}
     </div>
   )
 }
