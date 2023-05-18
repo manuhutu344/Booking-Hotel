e631484c7807bb80f88dba9bab57ee9137106518
manuhutu344 <nandoperdos2@gmail.com>
2023-05-18 19:36:46 +0900

    Membuat fitur lihat semua foto

--- src/Pages/PlacePage.jsx
+++ src/Pages/PlacePage.jsx
@@ -5,6 +5,7 @@ import { Link, useParams } from 'react-router-dom'
 function PlacePage() {
     const {id} = useParams()
     const [place, setPlace] = useState(null)
+    const [showAllPhotos, setShowAllPhotos] = useState(false)
     useEffect(()=>{
         if(!id){
             return
@@ -14,6 +15,30 @@ function PlacePage() {
         })
     }, [id])
     if(!place) return ''
+
+    if (showAllPhotos){
+      return(
+        <div className='absolute inset-0 min-h-screen'>
+        <div className='p-8 grid gap-4 bg-black text-white'>
+        <div>
+        <h2 className='text-3xl'>Semua Foto Dari {place.title}</h2>
+        <button onClick={() => setShowAllPhotos(false)} className='fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
+        Tutup semua foto
+        </button>
+        </div>
+        {place?.photos?.length > 0 && place.photos.map(photo => (
+          <div>
+          <img src={'http://localhost:4000/upload/'+photo} alt='' />
+          </div>
+        ))}
+        </div>
+        </div>
+      )
+    }
+
   return (
     <div className='mt-4 bg-gray-100 -mx-8 px-8 py-8'>
     <h1 className='text-3xl'>
@@ -42,7 +67,7 @@ function PlacePage() {
     </div>
     </div>
     </div>
-    <button className='flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500'>
+    <button onClick={() => setShowAllPhotos(true)} className='flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500'>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
 </svg>
