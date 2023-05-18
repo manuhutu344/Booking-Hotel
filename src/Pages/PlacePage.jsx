920b947c48f56e236004de5100e3bfa20f1098fd
manuhutu344 <nandoperdos2@gmail.com>
2023-05-18 19:47:49 +0900

    Menambahkan icon map

--- src/Pages/PlacePage.jsx
+++ src/Pages/PlacePage.jsx
@@ -44,7 +44,11 @@ function PlacePage() {
     <h1 className='text-3xl'>
     {place.title}
     </h1>
-    <Link className='my-2 block font-semibold underline' target='_blank' to={'https://maps.google.com/?q='+place.address}>
+    <Link className='flex gap-1 my-3 block font-semibold underline' target='_blank' to={'https://maps.google.com/?q='+place.address}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+</svg>
     {place.address}
     </Link>
     <div className='relative'>
