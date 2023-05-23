88bfe287314071232f54f678d783e7d9b8d6307a
manuhutu344 <nandoperdos2@gmail.com>
2023-05-23 20:18:19 +0900

    Mempercantik tampilan pada extra infonya

--- src/Pages/PlacePage.jsx
+++ src/Pages/PlacePage.jsx
@@ -41,7 +41,7 @@ function PlacePage() {
     }
 
   return (
-    <div className='mt-4 bg-gray-100 -mx-8 px-8 py-8'>
+    <div className='mt-4 bg-gray-100 -mx-8 px-8 pt-8'>
     <h1 className='text-3xl'>
     {place.title}
     </h1>
@@ -79,7 +79,7 @@ function PlacePage() {
     Lihat Lebih Banyak Foto
     </button>
     </div>
-    <div className='mt-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]'>
+    <div className='mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]'>
     <div>
     <div className='my-4'>
     <h2 className='font-semibold text-2xl'>
@@ -90,11 +90,18 @@ function PlacePage() {
     Check-in: {place.checkIn}<br />
     Check-out: {place.checkOut}<br />
     Maximal Pengunjung: {place.maxGuests}
-    <div className='mt-2 text-sm text-gray-700 leading-4'>{place.extraInfo}</div>
     </div>
     <div>
     <BokingWidget place={place} />
     </div>
+    </div>
+    <div className='bg-white -mx-8 px-8 py-8 border-t'>
+    <div>
+    <h2 className='font-semibold text-2xl'>
+    Info Tambahan
+    </h2>
+    </div>
+    <div className='mb-4 mt-1 text-sm text-gray-700 leading-5'>{place.extraInfo}</div>
     </div>
     </div>
   )
