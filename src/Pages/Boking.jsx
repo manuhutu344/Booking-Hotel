084d5e6e8003470b351314ccb72fa25c611cf57a
Candy <102036940+manuhutu344@users.noreply.github.com>
2023-05-31 23:51:02 +0900

    Membuat tampikan akun/bookings/${id}

--- src/Pages/Boking.jsx
+++ src/Pages/Boking.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import AddressLink from '../Components/AddressLink'
+import PlaceGalery from '../Components/PlaceGalery'
+import BookingDate from '../Components/BookingDate'
 
 function Boking() {
     const {id} = useParams()
+    const [booking, setBooking] = useState(null)
+    useEffect(()=>{
+      if(id){
+        axios.get('/post/bokings').then(response =>{
+        const foundbooking = response.data.find(({_id})=>_id === id)
+        if(foundbooking){
+          setBooking(foundbooking)
+        }
+        })
+      }
+    },[id])
+    if(!booking){
+      return ''
+    }
+
   return (
-    <div>Boking {id}</div>
+    <div className='my-8'>
+    <h1 className='text-3xl'>{booking.place.title}</h1>
+    <AddressLink className='my-2 block'>{booking.place.address}</AddressLink>
+    <div className='bg-gray-200 p-6 my-6 rounded-2xl flex items-center justify-between'>
+    <div>
+    <h2 className='text-2xl mb-4'>Informasi Tentang Bokingan Anda:</h2>
+    <BookingDate booking={booking} />
+    </div>
+    <div className='bg-primary p-6 text-white rounded-2xl'>
+   <div>Total Semua :</div>
+   <div className='text-3xl'>RP.{booking.place.price}</div>
+    </div>
+    </div>
+    <PlaceGalery place={booking.place} />
+    </div>
   )
 }
 
--- src/Pages/Bokings.jsx
+++ src/Pages/Bokings.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import AccountNav from '../Components/AccountNav'
 import axios from 'axios'
-import { differenceInCalendarDays, format } from 'date-fns'
 import PlaceImg from '../Components/PlaceImg'
+import { Link } from 'react-router-dom'
+import BookingDate from '../Components/BookingDate'
 
 function Bokings() {
   const [bookings, setBookings] = useState([])
@@ -16,7 +17,7 @@ function Bokings() {
     <AccountNav />
     <div>
     {bookings?.length > 0 && bookings.map(booking => (
-      <div className='flex gap-4 bg-gray-200 rounded-2xl overflow-hidder'>
+      <Link to={`/akun/bookings/${booking._id}`} className='flex gap-4 bg-gray-200 rounded-2xl overflow-hidder'>
       <div className='w-48'>
       <PlaceImg place={booking.place} />
       </div>
@@ -24,37 +25,19 @@ function Bokings() {
       <h2 className='text-xl'>
       {booking.place.title}
       </h2>
-      <div className='flex gap-2 items-center border-t border-gray-300 mt-2 py-2'>
-      <div className='flex gap-1 items-center'>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
-</svg>
-      {format(new Date(booking.checkIn), 'yyyy-MM-dd')} 
-      </div>
-      &rarr; 
-      <div className='flex gap-1 items-center'>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
-</svg>
-      {format(new Date(booking.checkOut), 'yyyy-MM-dd')}
-      </div>
-      </div>
       <div className='text-xl'>
+      <BookingDate booking={booking} className='mb-2 mt-4 text-gray-500' />
       <div className='flex gap-1'>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
-</svg>
-      {differenceInCalendarDays(new Date(booking.checkOut), new Date(booking.checkIn))} Hari
-      </div>
-      <div className='flex gap-1'>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
   <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
 </svg>
- Total Semua RP.{booking.price}
+<span className='text-2xl'>
+Total Semua: RP.{booking.price}
+</span>
       </div>
       </div>
       </div>
-      </div>
+      </Link>
     ))}
     </div>
     </div>
