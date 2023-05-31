102bf436a055a91cf8f38e59a097ce57b96710b5
Candy <102036940+manuhutu344@users.noreply.github.com>
2023-05-31 22:20:00 +0900

    Mempercantik halaman bokingan saya

--- src/Pages/Bokings.jsx
+++ src/Pages/Bokings.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import AccountNav from '../Components/AccountNav'
 import axios from 'axios'
+import { differenceInCalendarDays, format } from 'date-fns'
 import PlaceImg from '../Components/PlaceImg'
 
 function Bokings() {
@@ -15,11 +16,22 @@ function Bokings() {
     <AccountNav />
     <div>
     {bookings?.length > 0 && bookings.map(booking => (
-      <div>
-      <div>
+      <div className='flex gap-4 bg-gray-200 rounded-2xl overflow-hidder'>
+      <div className='w-48'>
       <PlaceImg place={booking.place} />
       </div>
-      {booking.checkIn}  {booking.checkOut}
+      <div className='py-3 pr-3 grow'>
+      <h2 className='text-xl'>
+      {booking.place.title}
+      </h2>
+      <div className='border-t border-gray-300 mt-2 py-2'>
+      {format(new Date(booking.checkIn), 'yyyy-MM-dd')} Sampai  {format(new Date(booking.checkOut), 'yyyy-MM-dd')}
+      </div>
+      <div>
+      Total Penginap : {differenceInCalendarDays(new Date(booking.checkOut), new Date(booking.checkIn))}<br/>
+     Total Semua RP.{booking.price}
+      </div>
+      </div>
       </div>
     ))}
     </div>
